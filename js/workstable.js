4c22caaf838716e0452c8e9996d3d53cc02a2e88
JeronimoCarranza <jeronimo.carranza@asterionat.com>
2018-06-19 04:45:32 +0200

    first upload

---
+++ js/workstable.js
@@ -0,0 +1,19 @@
+var filtersConfig = {
+    base_path: '/js/tablefilter/',
+    responsive: true,
+    col_widths: ['60px', '350px', '250px','100px'],
+    /** Bootstrap integration */
+    //  allows Bootstrap table styling
+    themes: [{
+        name: 'transparent'
+    }],
+	// grid layout customisation
+    grid_layout: {
+        width: '780px',
+        height: '330px',
+        filters: false,
+    },
+};
+
+var tf = new TableFilter(document.querySelector("table"), filtersConfig);
+tf.init();
